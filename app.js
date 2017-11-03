23c32ddf8d7bd347839b30f63823d9814512e72f
Tobias Genannt <t.genannt@scanplus.de>
2017-11-03 15:21:36 +0100

    Some stats

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var perHour = require('./routes/perHour');
+var perWeekday = require('./routes/perWeekday');
 
 var app = express();
 
@@ -22,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/perHour', perHour);
+app.use('/perWeekday', perWeekday);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/perHour.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var dbConn = require('../database/mongo-connection.js');
+var callLogModel = require('../database/CallLog-model.js');
+var mongoose = require('mongoose');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var db = dbConn();
+  db.once('open', function() {
+    var CallLog = callLogModel(mongoose, db);
+    var query = CallLog.aggregate([
+      {
+        $project: {
+          hour: {$hour: "$callDate"}
+        }
+      }, {
+        $group: {
+          _id: "$hour" ,
+          count: { $sum: 1 }
+        }
+      }
+    ]);
+
+    query.exec(function(err, queryResult) {
+      if (err) console.log(err);
+      res.render('perHour', { queryResult: queryResult });
+    });
+  });
+});
+
+module.exports = router;
