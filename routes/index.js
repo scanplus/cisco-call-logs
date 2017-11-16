d584ae6cf4e7f712021ce94037e9d0f4223e1d8e
Tobias Genannt <t.genannt@scanplus.de>
2017-11-16 07:35:31 +0100

    Better error handling

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ function loadCalls(filter, callback) {
     var query = CallLog.find(queryFilter).sort({callDate: -1}).limit(20);
 
     query.exec(function(err, queryResult) {
-      if (err) console.log(err, null);
+      if (err) callback(err, null);
       callback(null, queryResult);
     });
   });
@@ -30,18 +30,21 @@ function loadCalls(filter, callback) {
 
 router.get('/', function(req, res, next) {
   loadCalls('', function(err, queryResult) {
+    if (err) console.log(err);
     res.render('index', { queryResult: queryResult });
   });
 });
 
 router.get('/ext', function(req, res, next) {
   loadCalls('ext', function(err, queryResult) {
+    if (err) console.log(err);
     res.render('index', { queryResult: queryResult });
   });
 });
 
 router.get('/int', function(req, res, next) {
   loadCalls('int', function(err, queryResult) {
+    if (err) console.log(err);
     res.render('index', { queryResult: queryResult });
   });
 });
